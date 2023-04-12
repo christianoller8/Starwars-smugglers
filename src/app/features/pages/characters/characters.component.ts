280630043602b6e451e57ae51361d3d12a104923
Christian Oller <christianoller.dev@gmail.com>
2023-04-12 10:52:44 +0200

    Create smugglers page, add text and images.

--- src/app/features/pages/characters/characters.component.ts
+++ src/app/features/pages/characters/characters.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataCharactersService } from "src/app/core/services/data-characters.service";
+import { Character } from "src/app/core/models/character.interface";
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.scss']
+  selector: "app-characters",
+  templateUrl: "./characters.component.html",
+  styleUrls: ["./characters.component.scss"],
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit {
+  characters: Character[] = [];
 
+  constructor(private dataCharacters: DataCharactersService) {}
+
+  ngOnInit(): void {
+    this.characters = this.dataCharacters.getCharacters();
+  }
 }
