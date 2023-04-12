aa650163817ab5b6c72b5698e23a541b5eccf22c
Christian Oller <christianoller.dev@gmail.com>
2023-04-12 12:05:58 +0200

    Header and breadcrumbs added.

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { NavigationService } from "src/app/core/services/navigation.service";
+import { TranslateService } from "@ngx-translate/core";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+})
+export class HeaderComponent {
+  constructor(
+    public navigation: NavigationService,
+    public translate: TranslateService
+  ) {
+    translate.addLangs(["en", "es"]);
+    translate.setDefaultLang("en");
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+}
